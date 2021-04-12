eaaa0e9aeff25478520fbd009f88652e919a5ea5
Daniel Pape <danielwpape@gmail.com>
2021-04-12 12:56:35 +0100

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Daniel Pape on 12/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
